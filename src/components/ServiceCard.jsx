5df614135884ce6156c51c43863f674dfbe73d11
Prisha3108 <prishapatelm20@gmail.com>
2024-03-29 13:34:18 +0530

    Added a bit of service page

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/ServiceCard.css'
+
+const ServiceCard = ({ service }) => {
+    const { imgUrl, title, desc } = service;
+
+    return (
+        <div className="service__item">
+            <div className="service__img">
+                <img src={imgUrl} alt="" />
+            </div>
+            <h5>{title}</h5>
+            <p>{desc}</p>
+        </div>
+    );
+};
+
+export default ServiceCard;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
+import ServiceCard from './ServiceCard';
+import './css/Services.css';
 
-function Services() {
+import backgroundImage from '../assests/carimg.jpeg'; // Update the path to the background image
+import fuelImg from '../assests/fuelimage.jpeg';
+import batteryImg from '../assests/batteryimage.jpeg'; // Update the path to the battery image
+import towImg from '../assests/towimage.jpeg';
+import tyreImg from '../assests/tyreimage.webp';
+
+const servicesData = [
+    {
+        imgUrl: fuelImg,
+        title: "Fuel",
+        desc: "Get fuel delivery service on the spot."
+    },
+    {
+        imgUrl: batteryImg,
+        title: "Battery Change",
+        desc: "Get your vehicle's battery replaced wherever you are."
+    },
+    {
+        imgUrl: towImg,
+        title: "Tow the Car",
+        desc: "Tow your vehicle to the nearest service center."
+    },
+    {
+        imgUrl: tyreImg,
+        title: "Tyre Change",
+        desc: "Replace your vehicle's tyre at your location."
+    }
+];
+
+const Services = () => {
     return (
-        <div>
+        <div className="services-container">
+            <div className="header-image" style={{backgroundImage: `url(${backgroundImage})`}}>
+                {/* You can place additional content here if needed */}
+            </div>
             <h2>Our Services</h2>
-            <p>Discover the services we offer:</p>
-            <ul>
-                <li>Service 1</li>
-                <li>Service 2</li>
-                <li>Service 3</li>
-                {/* Add more services as needed */}
-            </ul>
+            <div className="service__list">
+                {servicesData.map((service, index) => (
+                    <ServiceCard key={index} service={service} />
+                ))}
+            </div>
         </div>
     );
-}
+};
 
 export default Services;
